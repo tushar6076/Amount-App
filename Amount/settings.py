06d8053814394051358a2bf6ede05ee56be23f42
Tushar <136443505+tushar6076@users.noreply.github.com>
2025-10-02 00:35:53 +0530

    Delete Amount directory

--- Amount/settings.py
+++
@@ -1,9 +0,0 @@
-from kivymd.app import MDApp
-from kivymd.uix.screen import MDScreen
-
-
-class SettingsScreen(MDScreen):
-
-    def home_button(self, instance):
-        app = MDApp.get_running_app()
-        app.screen_manager.current = "home"
--- Amount/test.py
+++
@@ -1,5 +0,0 @@
-import datetime
-
-start_date = datetime.date.today()
-seven_day_dates = [start_date + datetime.timedelta(days=i) for i in range(7)]
-print(seven_day_dates)
